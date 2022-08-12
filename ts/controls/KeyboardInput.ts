d60adda450e6316e8462b5973f6720caf8ba81cc
Liz Ainslie <lizainslie16@gmail.com>
2022-08-12 11:43:44 -0700

    add keyboard input utility

--- ts/controls/KeyboardInput.ts
+++ ts/controls/KeyboardInput.ts
@@ -1,4 +1,10 @@
 export class KeyboardInput {
+  private pressedKeys: Set<string> = new Set();
+  private ctrlPressed: boolean = false;
+  private shiftPressed: boolean = false;
+  private altPressed: boolean = false;
+  private metaPressed: boolean = false;
+
   constructor() {
     this.keyDownHandler = this.keyDownHandler.bind(this);
     this.keyUpHandler = this.keyUpHandler.bind(this);
@@ -7,9 +13,38 @@ export class KeyboardInput {
     document.addEventListener('keyup', this.keyUpHandler);
   }
 
-  keyDownHandler(event: KeyboardEvent) {}
+  private keyDownHandler(event: KeyboardEvent) {
+    this.pressedKeys.add(event.key);
 
-  keyUpHandler(event: KeyboardEvent) {}
+    this.ctrlPressed = event.ctrlKey;
+    this.altPressed = event.altKey;
+    this.shiftPressed = event.shiftKey;
+    this.metaPressed = event.metaKey;
+  }
+
+  private keyUpHandler(event: KeyboardEvent) {
+    this.pressedKeys.delete(event.key);
+  }
+
+  isKeyPressed(key: string): boolean {
+    return this.pressedKeys.has(key);
+  }
+
+  get isCtrlPressed(): boolean {
+    return this.ctrlPressed;
+  }
+
+  get isAltPressed(): boolean {
+    return this.altPressed;
+  }
+
+  get isShiftPressed(): boolean {
+    return this.shiftPressed;
+  }
+
+  get isMetaPressed(): boolean {
+    return this.metaPressed;
+  }
 }
 
 export default KeyboardInput;
