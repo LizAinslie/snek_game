18df857b6683da775d981ff89afe5719b230382b
Liz Ainslie <lizainslie16@gmail.com>
2022-08-13 12:10:20 -0700

    Add sprinting, and keyboard controls to CharacterController

--- ts/CharacterController.ts
+++ ts/CharacterController.ts
@@ -8,6 +8,7 @@ export class CharacterController {
   movementSpeed: number = 100;
   joystickThreshold: number = 0.25;
   keyboardInput: KeyboardInput = new KeyboardInput();
+  sprintModifier: 1.35;
 
   constructor(
     sprite: Sprite,
@@ -28,11 +29,13 @@ export class CharacterController {
       const rawXAxis = this.gamepadAPI.axesStatus[0];
       const rawYAxis = this.gamepadAPI.axesStatus[1];
 
+      const sprintMod: number = this.gamepadAPI.buttonPressed('LS') ? this.sprintModifier : 1;
+
       if (
         rawXAxis > this.joystickThreshold ||
         rawXAxis < -this.joystickThreshold
       ) {
-        this.sprite.movementVector.x = rawXAxis * 2 * this.movementSpeed;
+        this.sprite.movementVector.x = rawXAxis * 2 * this.movementSpeed * sprintMod;
       } else this.sprite.movementVector.x = 0;
 
       // if (this.sprite.movementVector.x < 0 && this.sprite.pos.x <= 1)
@@ -47,7 +50,7 @@ export class CharacterController {
         rawYAxis > this.joystickThreshold ||
         rawYAxis < -this.joystickThreshold
       ) {
-        this.sprite.movementVector.y = rawYAxis * 2 * this.movementSpeed;
+        this.sprite.movementVector.y = rawYAxis * 2 * this.movementSpeed * sprintMod;
       } else this.sprite.movementVector.y = 0;
 
       // if (this.boxYVector < 0 && this.boxY <= 1) this.boxYVector = 0;
@@ -57,6 +60,30 @@ export class CharacterController {
       // )
       //   this.boxYVector = 0;
     }
+
+    let keyboardXValue = 0;
+    let keyboardYValue = 0;
+
+    const upPressed = this.keyboardInput.isKeyPressed('w') || this.keyboardInput.isKeyPressed('ArrowUp');
+    const leftPressed = this.keyboardInput.isKeyPressed('a') || this.keyboardInput.isKeyPressed('ArrowLeft');
+    const downPressed = this.keyboardInput.isKeyPressed('s') || this.keyboardInput.isKeyPressed('ArrowDown');
+    const rightPressed = this.keyboardInput.isKeyPressed('d') || this.keyboardInput.isKeyPressed('ArrowRight');
+
+    if (upPressed) keyboardYValue = -1;
+    if (leftPressed) keyboardXValue = -1;
+    if (downPressed) keyboardYValue = 1;
+    if (rightPressed) keyboardXValue = 1;
+
+    if (upPressed && downPressed) keyboardYValue = 0;
+    if (leftPressed && rightPressed) keyboardXValue = 0;
+
+    if (this.keyboardInput.isShiftPressed) {
+      keyboardXValue *= this.sprintModifier;
+      keyboardYValue *= this.sprintModifier;
+    }
+
+    this.sprite.movementVector.x = keyboardXValue * this.movementSpeed;
+    this.sprite.movementVector.y = keyboardYValue * this.movementSpeed;
   }
 }
 
